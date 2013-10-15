82d1c27e7c4ff8bf8abd60beb59d5e674cfff1bc
Michael Imamura <zoogie@lugatgt.org>
2013-10-14 20:14:59 -0400

    Add ResStr.

---
+++ src/ResStr.cpp
@@ -0,0 +1,68 @@
+
+/* ResStr.cpp
+ *
+ * Copyright (C) 2013 Michael Imamura
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+#include "StdAfx.h"
+
+#include <fstream>
+
+#include "Exception.h"
+
+#include "ResStr.h"
+
+namespace AISDL {
+
+ResStr::ResStr(const std::string &filename) :
+	filename(filename)
+{
+	Reload();
+}
+
+/**
+ * Create a new resource string.
+ * @param filename The file to load the string from.
+ * @return The resource string.
+ */
+std::shared_ptr<ResStr> ResStr::Load(const std::string &filename)
+{
+	//TODO: Keep weak_ptrs to returned assets for ReloadAll().
+	return std::make_shared<ResStr>(filename);
+}
+
+/**
+ * Reload the string from the file it came from.
+ */
+void ResStr::Reload()
+{
+	// Fast byte-for-byte string read.
+	// Adapted from:
+	// http://insanecoding.blogspot.com/2011/11/how-to-read-in-file-in-c.html
+	std::ifstream in(filename, std::ios::in | std::ios::binary);
+	if (in) {
+		in.seekg(0, std::ios::end);
+		s.resize(in.tellg());
+		in.seekg(0, std::ios::beg);
+		in.read(&s[0], s.size());
+		in.close();
+	}
+	else {
+		throw Exception("Failed to read: " + filename);
+	}
+}
+
+}  // namespace AISDL
+
---
+++ src/ResStr.h
@@ -0,0 +1,49 @@
+
+/* ResStr.h
+ *
+ * Copyright (C) 2013 Michael Imamura
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+#pragma once
+
+namespace AISDL {
+
+/**
+ * A file-backed string that can be reloaded at runtime.
+ * @author Michael Imamura
+ */
+class ResStr {
+	friend const std::string &operator*(const ResStr &str);
+public:
+	ResStr(const std::string &filename);
+
+public:
+	static std::shared_ptr<ResStr> Load(const std::string &filename);
+
+public:
+	void Reload();
+
+private:
+	std::string filename;
+	std::string s;
+};
+
+inline const std::string &operator*(const ResStr &str)
+{
+	return str.s;
+}
+
+}  // namespace AISDL
+
