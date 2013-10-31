e6f3b184c9b0f799fb856f818a87fe5c30bbb0b7
Michael Imamura <zoogie@lugatgt.org>
2013-10-31 16:44:20 -0400

    Allow starting scene to be set from command-line.

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,55 @@
 #include <SDL2/SDL_ttf.h>
 
 #include "App.h"
+#include "Config.h"
 #include "Exception.h"
 
 using namespace AISDL;
+
+/**
+ * Parse command-line options and set Config flags.
+ * @param argc The command-line argument count.
+ * @param argv The command-line arguments.
+ * @return @c true if options are okay,
+ *         @c false if option parsing was aborted.
+ * @throws Exception Invalid option encountered.
+ */
+static bool ProcessCommandLine(int argc, char **argv)
+{
+	Config &cfg = Config::instance;
+
+	for (int i = 1; i < argc; i++) {
+		const std::string arg = argv[i];
+
+		if (arg == "-h" || arg == "--help" || arg == "-?") {
+			std::cout <<
+				"Usage: adventures-in-sdl2 [options] [startingScene]\n"
+				"\n"
+				"  -h, --help, --?\n"
+				"    Print this help text.\n";
+			return false;
+		}
+		else {
+			bool validScene = false;
+			// Remaining argument is the starting scene index.
+			try {
+				cfg.startingScene = std::stoi(arg);
+				if (cfg.startingScene >= 0) {
+					validScene = true;
+				}
+			}
+			catch (...) {
+			}
+			if (!validScene) {
+				std::ostringstream oss;
+				oss << "Invalid starting scene index: " << arg;
+				throw Exception(oss.str());
+			}
+		}
+	}
+
+	return true;
+}
 
 /// Initialize each of the SDL subsystems.
 static void InitApp()
@@ -67,9 +113,26 @@ int main(int argc, char** argv)
 {
 	srand(static_cast<unsigned int>(time(nullptr)));
 
+#ifdef _WIN32
+	int vargc = __argc;
+	char **vargv = __argv;
+#else
+	int vargc = argc;
+	char **vargv = argv;
+#endif
+	try {
+		if (!ProcessCommandLine(vargc, vargv)) {
+			return 0;
+		}
+	}
+	catch (Exception &ex) {
+		std::cerr << ex.what() << std::endl;
+		return 1;
+	}
+
 	try {
 		InitApp();
-		App().Run();
+		App(Config::instance.startingScene).Run();
 	}
 	catch (Exception &ex) {
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
