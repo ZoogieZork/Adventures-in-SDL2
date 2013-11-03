56444d8b6b97204d11758c819be4b754bd3f38f5
Michael Imamura <zoogie@lugatgt.org>
2013-11-03 00:49:56 -0400

    Add level BG loading and rendering.

---
+++ src/Level.h
@@ -0,0 +1,46 @@
+
+/* Level.h
+ *
+ * Copyright (C) 2013 Michael Imamura
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+#pragma once
+
+#include "Layer.h"
+
+namespace AISDL {
+
+class Level {
+public:
+	Level(const std::string &dirname);
+
+public:
+	static std::shared_ptr<Level> Load(const std::string &filename);
+	void Reload();
+
+public:
+	size_t GetWidth() const { return width; }
+	size_t GetHeight() const { return height; }
+	size_t GetNumLayers() const { return layers.size(); }
+	const Layer &GetLayer(size_t i) const { return layers[i]; }
+
+private:
+	const std::string dirname;
+	std::vector<Layer> layers;
+	size_t width;
+	size_t height;
+};
+
+}  // namespace AISDL
